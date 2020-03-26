0a6a835dafae88846949c0624c4403bbb3931706
Tom Elsey <telsey@toms-air.home>
2020-03-25 20:53:04 -0700

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,7 @@
+console.log("up and running!");
+let card1 = "queen";
+let card2 = "queen";
+let card3 = "king";
+let card4 = "king";
+console.log("User flipped " + card1);
+console.log("User flipped " + card2);
