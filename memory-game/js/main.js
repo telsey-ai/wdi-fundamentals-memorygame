4c1f591a2d6c4965e4f7348f97cc9f115c00711d
Tom Elsey <telsey@Toms-MacBook-Air.local>
2020-04-03 21:54:10 -0400

    created gamelogic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -21,25 +21,38 @@ let cards = [
 }
 ];
 let cardsInPlay = [];
+function createBoard(){
+	for(i=0;i<cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
 function checkForMatch(){
 	if(cardsInPlay[0]===cardsInPlay[1]){
-			console.log("you found a match!");
+			alert("you found a match!");
 		}else{
-			console.log("Sorry, try again");
+			alert("Sorry, try again");
 		}
 }
 
-function flipCard(cardId){
+function doesNothing(){
+
+}
+function flipCard(){
+	let cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
-	
+	//console.log(cards[cardId].cardImage);
+	//console.log(cards[cardId].suit);
+	this.setAttribute('src', cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
+	
 	if(cardsInPlay.length===2){
 		checkForMatch();
 	}
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
